6690d2d37f6fee215a050b129fb9345b2b55f73d
Singing-Han <1563395763@qq.com>
2020-12-24 21:55:17 +0800

    构建门户 --> 2020-12-24 下午

---
+++ taotao-portal/src/main/java/cn/edu/sziit/controller/PageController.java
@@ -0,0 +1,18 @@
+package cn.edu.sziit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/{path}")
+    public String page(@PathVariable(name="path")String path){
+//        System.out.println("=======" + path);
+        return path;
+    }
+
+
+
+}
